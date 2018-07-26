bce7baeb1821ddfca0d5ce1e2ac62065a4176d61
Nitesh Ranjan <niteshr@INniteshr.local>
2018-07-26 22:52:43 +0530

    Getting repos of the given username from github

---
+++ index.js
@@ -0,0 +1,34 @@
+const https = require('https');
+
+const username = process.argv[2];
+
+const getOptions = function(){
+  const options = {
+    hostname: 'api.github.com',
+    path: `/users/${username}/repos`,
+    //Insert your Github access_token below :-
+    access_token : 'XXX-XXX',
+    headers: {
+      'User-Agent': 'Request-Promise',
+    },
+  };
+  return options;
+}
+
+const getRepos = function(){
+  return new Promise((resolve,reject) => {
+    const req = https.get(getOptions(), (res) => {
+      let repos = '';
+      res.on('data', (chunk) => repos += chunk );
+      res.on('end',() => resolve(JSON.parse(repos)) );
+    })
+    req.on('error', (err) => reject(err));
+  })
+}
+
+const printRepos = (repos) => repos.forEach((repo) => console.log(repo.name))
+
+getRepos()
+  .then((repos)=>{
+    printRepos(repos);
+  })
